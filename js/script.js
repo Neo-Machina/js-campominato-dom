9935eaf400b0b7dfe4ad40577b6d43d28c0576fd
Neo-Machina <andreistatache1997@gmail.com>
2022-06-15 18:00:56 +0200

    step 1 completed

--- js/script.js
+++ js/script.js
@@ -10,3 +10,76 @@
 // BONUS:
 // 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
 // 2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
+
+
+const button = document.getElementById('btn-game');
+button.addEventListener('click', startGame)
+
+function startGame() {
+    // 1 - Chiediere un livello di difficoltà (1,2,3) all'utente, con un prompt 
+    // se si sceglie 1, il range di numeri possibili del gioco è 1-100
+    // se si sceglie 2, il range di numeri possibili del gioco è 1-81
+    // se si sceglie 3, il range di numeri possibili del gioco è 1-49
+    let userDifficultyLevel = document.getElementById('user-level-difficulty').value;
+    console.log(userDifficultyLevel);
+    
+    let gameMaxRange;
+    switch(userDifficultyLevel) {
+        case '1':
+            gameMaxRange = 100;
+            break;
+        case '2':
+            gameMaxRange = 81;
+            break;
+        case '3':
+            gameMaxRange = 49;
+            break;
+    }
+    
+    // 2- Generare 16 numeri random (bombe) 
+    // nello stesso range delle tre difficoltà:
+    // gameMinRange(1)-gameMaxRange(100 o 81 o 49)
+    const bombsNumber = 16;
+    const bombs = bombsGenerator(bombsNumber, 1, gameMaxRange);
+    console.log(bombs);
+    
+    // 3 - Numero max di tentativi possibili per ogni difficoltà scelta: gameMaxRange - bombe(16)
+    const maxAttemptsNumbers = gameMaxRange - bombsNumber;
+    console.log(maxAttemptsNumbers);
+    
+    // Array numeri indovinati     
+    const guessedArrayNumbers = []; 
+}
+
+// ------------------
+// UTILITY FUNCTIONS
+// ------------------
+    
+// Generare un array di tot numero di elementi random 
+// Inputs: 
+// numberOfArrayElements -> numero di elementi dell'array
+// rangeMin -> range minimo dei numeri random generati
+// rangeMax -> range massimo dei numeri random generati
+// Output:
+// return -> array di elementi random con lunghezza numberOfArrayElements
+function bombsGenerator(numberOfArrayElements, rangeMin, rangeMax) {
+    // Creazione array vuoto
+    const randomNumbersArray = [];
+    // Finchè la lunghezza dell'array è inferiore al numero di elementi dell'arrey
+    while(randomNumbersArray.length < numberOfArrayElements) {
+        // Genero un numero random da rangeMin a rangeMax
+        const randomNumber = getRndInteger(rangeMin, rangeMax);
+        // Inserisco il numero nell'array solo se esso non è già presente 
+        if(!randomNumbersArray.includes(randomNumber)) {
+                randomNumbersArray.push(randomNumber);
+        }
+        
+    }
+        
+    return randomNumbersArray;
+}
+        
+// Funzione per generare un numero random
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
